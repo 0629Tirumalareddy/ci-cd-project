540816bb07fc85af6002ec16bee9b2128c569171
Ubuntu <ubuntu@ip-172-31-24-199.ap-southeast-2.compute.internal>
2026-03-30 07:14:11 +0000

    After github webhook triggerd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello This is Tiru----------My Node.js app is running on EC2 instance");
+  res.end("Hello This is Tiru----------My Node.js app is running on EC2 instance and triggered to github webhook");
 });
 
 server.listen(3000, () => {
