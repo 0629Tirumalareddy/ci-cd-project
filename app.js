224a810f486752694fc84d1305efc7ff4f6a9004
Ubuntu <ubuntu@ip-172-31-24-199.ap-southeast-2.compute.internal>
2026-04-02 08:47:42 +0000

    Old commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
 
   if (req.url === "/") {
-    res.end("This is Tiru from Terminal my app is going onlineeeeeeeeeeeeee");
+    res.end("This is Hemanth from Terminal my app is going online");
   }
 
   else if (req.url === "/health") {
